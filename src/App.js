98aebf7c09ffde2bb3b87724838d5930390acca0
Eisenbartl <Eisenbartl@yahoo.com>
2020-07-04 09:40:11 -0500

    search feature added, mvp completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from './components/ui/header.js';
+import CharacterGrid from './components/characters/CharacterGrid.js';
+import Search from './components/ui/Search.js';
 import './App.css';
 
 const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
+
+      console.log(result.data);
+      setItems(result.data);
+      setIsLoading(false);
+    }
+
+    fetchItems();
+  }, [query])
+
   return (
     <div className="mainContainer">
-      <h1>turtle</h1>
+      <Header/>
+      <Search getQuery={q => setQuery(q)}/>
+      <CharacterGrid 
+        isLoading={isLoading}
+        items={items}
+      />
     </div>
   );
 }
---
+++ src/components/characters/CharacterGrid.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import CharacterItem from './CharacterItem.js';
+import Spinner from '../ui/Spinner.js';
+
+const CharacterGrid = ({ items, isLoading }) => {
+    return isLoading ? 
+        <Spinner /> 
+        : 
+        <section className="cards">
+            {items.map(item => (
+                <CharacterItem key={item.char_id} item={item}></CharacterItem>
+            ))}
+        </section>;
+}
+
+export default CharacterGrid;
